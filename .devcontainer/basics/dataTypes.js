e9749fd95d81f74ead8448f123ae0ce68d6246ab
sahilrana113 <84993246+sahilrana113@users.noreply.github.com>
2023-11-22 13:24:21 +0000

    commit 2

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,9 @@
+let name=" sahil"
+let age= 22
+let address =" himachal predesh"
+
+console.log(age)
+
+console.log( typeof "sahil")
+
+console.log( typeof age)
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,6 @@
+const a=1233
+
+console.log(a);
+
+a=12
+console.log(a);
